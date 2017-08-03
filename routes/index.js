5792d1392fd6564ba5584c2cfae18cc88bdf21c1
Alysson Martin <alysson.martin@grupointegrado.br>
2017-08-02 21:04:40 -0300

    Correção

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.send("Ola Node JS! Agora Com NodeMom!");
 });
 
-router.get('/', function(req, res, next) {
-  res.send("Teste NodeJS");
-});
-
 module.exports = router;
